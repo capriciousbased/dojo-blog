f0afdb1069cdb89361633a559d1588c34b1db58b
capriciousbased <mdanisulhoquekhan@gmail.com>
2022-09-19 21:41:02 +0200

    Fetch data and catch Error option added

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,36 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
-function Home() {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
+const Home = () => {
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("could not fetch the data for the resouces");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setIsPending(false);
+          setBlogs(data);
+        })
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.message);
+        });
+    }, 1000);
+  }, []);
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" />
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} />}
     </div>
   );
-}
+};
 
 export default Home;
