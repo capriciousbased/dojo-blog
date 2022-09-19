09262606bc20398df04328faa07ca53c1106b47d
capriciousbased <mdanisulhoquekhan@gmail.com>
2022-09-19 21:53:29 +0200

    Add Fetch data custom Hook

--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,13 @@
-import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-  useEffect(() => {
-    setTimeout(() => {
-      fetch("http://localhost:8000/blogs")
-        .then((res) => {
-          if (!res.ok) {
-            throw Error("could not fetch the data for the resouces");
-          }
-          return res.json();
-        })
-        .then((data) => {
-          setIsPending(false);
-          setBlogs(data);
-        })
-        .catch((err) => {
-          setIsPending(false);
-          setError(err.message);
-        });
-    }, 1000);
-  }, []);
-
+  const { data, isPending, error } = useFetch("http://localhost:8000/blogs");
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
-      {blogs && <BlogList blogs={blogs} />}
+      {data && <BlogList blogs={data} />}
     </div>
   );
 };
